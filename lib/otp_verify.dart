0dcef70e297bef411c97949e026d2bc33ad6d8ad
Deepanjali Tripathi <deepanjalitripathi7@gmail.com>
2022-08-04 19:14:12 +0530

    c

--- lib/otp_verify.dart
+++ lib/otp_verify.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class otpVerify extends StatefulWidget {
   const otpVerify({Key? key}) : super(key: key);
@@ -11,7 +12,143 @@ class _otpVerifyState extends State<otpVerify> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Hello World')),
+      // backgroundColor: Colors.white,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Container(
+            padding: EdgeInsets.only(top: 100),
+            child: Column(
+              children: [
+                Center(
+                  child: Text(
+                    'OTP',
+                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Center(
+                  child: Text(
+                    'Verification',
+                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Center(
+                  child: Text(
+                    'OTP has been sent to +91 9876543210',
+                    style: TextStyle(
+                      fontSize: 15,
+                      color: Colors.black87,
+                    ),
+                  ),
+                ),
+                Container(
+                  child: Row(
+                    children: [
+                      SizedBox(
+                        height: 68,
+                        width: 64,
+                        child: TextFormField(
+                          style: Theme.of(context).textTheme.headline6,
+                          keyboardType: TextInputType.number,
+                          textAlign: TextAlign.center,
+                          inputFormatters: [
+                            LengthLimitingTextInputFormatter(1),
+                            FilteringTextInputFormatter.digitsOnly,
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 68,
+                        width: 64,
+                        child: TextFormField(
+                          style: Theme.of(context).textTheme.headline6,
+                          keyboardType: TextInputType.number,
+                          textAlign: TextAlign.center,
+                          inputFormatters: [
+                            LengthLimitingTextInputFormatter(1),
+                            FilteringTextInputFormatter.digitsOnly,
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 68,
+                        width: 64,
+                        child: TextFormField(
+                          style: Theme.of(context).textTheme.headline6,
+                          keyboardType: TextInputType.number,
+                          textAlign: TextAlign.center,
+                          inputFormatters: [
+                            LengthLimitingTextInputFormatter(1),
+                            FilteringTextInputFormatter.digitsOnly,
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 68,
+                        width: 64,
+                        child: TextFormField(
+                          style: Theme.of(context).textTheme.headline6,
+                          keyboardType: TextInputType.number,
+                          textAlign: TextAlign.center,
+                          inputFormatters: [
+                            LengthLimitingTextInputFormatter(1),
+                            FilteringTextInputFormatter.digitsOnly,
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 68,
+                        width: 64,
+                        child: TextFormField(
+                          style: Theme.of(context).textTheme.headline6,
+                          keyboardType: TextInputType.number,
+                          textAlign: TextAlign.center,
+                          inputFormatters: [
+                            LengthLimitingTextInputFormatter(1),
+                            FilteringTextInputFormatter.digitsOnly,
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 68,
+                        width: 64,
+                        child: TextFormField(
+                          style: Theme.of(context).textTheme.headline6,
+                          keyboardType: TextInputType.number,
+                          textAlign: TextAlign.center,
+                          inputFormatters: [
+                            LengthLimitingTextInputFormatter(1),
+                            FilteringTextInputFormatter.digitsOnly,
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                // SizedBox(
+                //   height: 300,
+                // ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(6),
+                    ),
+                    minimumSize: Size(320, 60),
+                  ),
+                  onPressed: () {},
+                  child: Text(
+                    'Continue',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
+                Container(),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
